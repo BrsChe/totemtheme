a575a57ab9911973a0131a5eff14cf66da10edc8
Boris Chebar <boryachebar@gmail.com>
2023-04-25 23:26:16 +0500

    Добавил иконки в футере и в контактах

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer class="body_foot">
 	<div class="info_footer container">
 		<div class="info_contact info_2">
-			<div class="info_tel info_all"><a href="tel:+79017548262">+ 7 (901) 754 82 62</a></div>
+			<div class="info_tel info_all"><a href="tel:+79017548262">+7 (901) 754 82 62</a></div>
 			<div class="info_mail info_all"><a href="mailto:info@legal-union.ru">info@legal-union.ru</a></div>
 		</div>
 		<div class="info_cookie info_2">Следите за новостями и обзорами в социальных сетяx</div>
@@ -15,4 +15,5 @@
                <a href="https://www.facebook.com/legalsideproperty">
                   <img src="<?php echo get_template_directory_uri() . '/images/totem/fb.svg';?>" alt="Фейсбук" title="Фейсбук">
 	<?php wp_footer(); ?>
-</footer>
+   </footer>
+
