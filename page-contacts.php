253781a2e141762a7c8eed433a5e1985009f5502
Lev Koshak <levvvkoshak@gmail.com>
2023-05-03 18:50:30 +0500

    Адаптив торгов, новостей, переделал немного адаптив на главной, поверстал новости, заверстал первую услугу (сопровождение сделок)

---
+++ page-contacts.php
@@ -0,0 +1,17 @@
+<div class="contacts_inner">
+   <div class="contacts_image">
+      <img
+         src="http://wordpress/wp-content/themes/totemtheme/images/totem/OlgaNaumova.jpg"
+         alt="OlgaNaumova"
+         title="OlgaNaumova">
+   </div>
+   <div class="contacts_info">
+      <h4 class="p-s w-full gradient-bg-red font-century">ЮРИСТ</h4>
+      <h3 class="p-s font-color-red wp-block-heading contacts_FIO">Наумова Ольга Владимировна</h3>
+      <p class="p-s has-text-align-left">
+         Более 15 лет работы в сфере недвижимости, земельных, жилищных правоотношений, вопросов строительства. Опыт работы в Росреестре, суде, строительных компаниях.
+      </p>
+      <p class="p-s con-opt"><a class="con-opt-phone" href="tel:+79017548262">+7 (901) 754 82 62</a></p>
+      <p class="p-s con-opt"><a class="con-opt-mail" href="nov7548262@gmail.com">nov7548262@gmail.com</a></p>
+   </div>
+</div>
--- page-news.php
+++ page-news.php
@@ -3,74 +3,27 @@
  * Template name: News
  */
 ?>
-<?php
-get_header();
-?>
+<?php get_header(); ?>
 <?php if (have_posts()): while (have_posts()): the_post(); ?>
     <?php the_content(); ?>
 <?php endwhile; endif; ?>
-
-        <section id="news">
-            <h1 class="news-title">ПРАВОВЫЕ НОВОСТИ И ОБЗОРЫ ПО ВОПРОСАМ НЕДВИЖИМОСТИ, ЗЕМЛИ СТРОИТЕЛЬСТВА</h1>
-            <div class="main-inner">
-                <div class="center">
-                    <section class="module-6">
-                        <div class="module-inner">
-			
-			
-			
-			<?php	
-			
-			 $args1 = array(
-            'paged'          => $paged,
-            'posts_per_page' => 10,
-            'cat' => 3,
-        );
-        $query1 = new WP_Query( $args1 );
-			
-			  while ( $query1->have_posts() ) : $query1->the_post(); ?>
-                            <article class="item">
-                                <?php   if ( has_post_thumbnail() ) {
-	                                        the_post_thumbnail();
-                                        }  ?>
-				                <a class="title" href="<?php the_permalink(); ?>"><h3><?php the_title(); ?></h3></a>
-				                <div class="date"><?php echo the_date('d.m.Y'); ?></div>
-                                <div class="readmore">
-                                    <a role="button" href="<?php the_permalink(); ?>">Подробнее</a>
-                                </div>
-                            </article>
-			<?php endwhile; wp_reset_query(); ?>
-
-                        </div>
-						<?php
-						$pag_args1 = array(
-            'current' => max( 1, get_query_var('paged') ),
-            'total'   => $query1->max_num_pages,
-'show_all'           => False,
-'prev_text'          => __('Назад'),
-	'next_text'          => __('Далее')
-        ); ?>
-		<div class="navigation">
-		<? echo paginate_links( $pag_args1 ); ?>
-		</div>
-                    </section>
+<div id="news" class="home_news box_shadow">
+    <div class="heading"><?php if (!function_exists('dynamic_sidebar') || !dynamic_sidebar("Top phone")) ?></div>
+    <div class="home_news_wrapper container">
+        <div class="home_news_content">
+            <?php if (have_posts()) : query_posts("cat=4"); while (have_posts()): the_post(); ?>
+            <div class="home_news-item">
+                <a class="home_news-item_image" href="<?php the_permalink(); ?>">
+                    <?php if ( has_post_thumbnail() ) { the_post_thumbnail(); }?>
+                </a>
+                <div class="home_news-item_title item_title">
+                    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
                 </div>
+                <div class="home_news-item_date color-dark_gray"><?php echo the_date('d.m.Y'); ?></div>
+                <a class="moreinfobtn gradient-bg-red" href="<?php the_permalink(); ?>">Подробнее</a>
             </div>
-        </section>
-        <div class="soc_seti">
-        <div class="body_foot"><div class="soc_title">Следите за новостями и обзорами в социальных сетяx</div>
-                <div class="soc_img">
-            <div class="soc_vk soc_style">
-                <a href="#"><img class="soc_img_2" src="../wp-content/uploads/2022/08/vk.png" alt=""></a>
-            </div>
-            <div class="soc_tg soc_style">
-                <a href="#"><img class="soc_img_2" src="../wp-content/uploads/2022/08/tg.png" alt=""></a>
-            </div>
-            <div class="soc_facebook soc_style">
-                <a href="#"><img class="soc_img_2" src="../wp-content/uploads/2022/08/facebook.png" alt=""></a>
-            </div>
+            <?php endwhile; wp_reset_query(); endif; ?>
         </div>
-    </div></div>
-<?php
-get_footer();
-?>
+    </div>
+</div>
+<?php get_footer(); ?>
